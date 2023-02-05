643494daaa10ccc2ce7b7c6d28d778462c871c2a
ygoto3 <my.important.apes@gmail.com>
2023-02-05 15:12:31 +0900

    Enable cloud run api

--- terraform/server/main.tf
+++ terraform/server/main.tf
@@ -1,4 +1,13 @@
+# Enables the Cloud Run API
+resource "google_project_service" "run_api" {
+  service = "run.googleapis.com"
+
+  disable_on_destroy = true
+}
+
 module "cloud_run" {
+  depends_on = [google_project_service.run_api]
+
   source = "../modules/cloud_run"
 
   project_id = "${var.project_id}"
@@ -6,3 +15,4 @@ module "cloud_run" {
   location = "${var.region}"
   image_id = "${var.image_id}"
 }
+
